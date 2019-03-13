c7d2ce407f580f73234e063655217d0220d0d6cd
russ3llwong <russellwong97@hotmail.com>
2019-03-12 19:36:23 -0700

    dummy comments generated by selecting/tapping posts

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,28 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Russell Wong on 3/12/19.
+//  Copyright © 2019 Russell Wong. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
